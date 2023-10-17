b4255c2b07f60aa79fca57767d19d9e427ff59cd
Em Lowing <elowing@gmail.com>
2023-10-17 12:20:44 -0400

    Update scripts

--- dot_dotfiles/install.sh
+++ dot_dotfiles/install.sh
@@ -9,4 +9,10 @@ brew install --cask datweatherdoe
 brew install powerlevel10k
 brew install ripgrep
 
+chezmoi init https://github.com/elowing/dotfiles.git
+echo 'Run `chezmoi diff` to see changes to be made to $HOME'
+echo 'If those are good, run `chezmoi apply -v`'
+echo 'If not, run `chezmoi edit $FILE`'
+echo 'To pull and update anytime, run `chezmoi update -v`'
+
 echo -e "\n. $(brew --prefix asdf)/libexec/asdf.sh" >> ${ZDOTDIR:-~}/.zshrc
